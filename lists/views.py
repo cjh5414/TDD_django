1eca76b4247126d74e7a9bdecd3ba8caa4708b9a
cjh5414 <cjh5414@gmail.com>
2016-09-19 22:52:22 +0900

    List model 추가, 관련된 테스트, 코드 수정

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
@@ -13,5 +13,6 @@ def view_list(request):
 
 @csrf_exempt
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/the-only-list-in-the-world/')
