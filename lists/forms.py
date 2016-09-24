69d55e3fbff6127c2928dfe8b99752405911eb28
cjh5414 <cjh5414@gmail.com>
2016-09-24 19:11:11 +0900

    모든 아이템의 입력 id와 name 변경. 아직 망가진 상태

--- lists/forms.py
+++ lists/forms.py
@@ -11,7 +11,7 @@ class ItemForm(forms.models.ModelForm):
 		fields = ('text',)
 		widgets = {
 			'text': forms.fields.TextInput(attrs={
-			'placeholder': '작업 아이템 입력',
+			'placeholder': 'Enter a to-do item',
 			'class': 'form-control input-lg',
 			}),
 		}
