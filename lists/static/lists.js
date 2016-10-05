a7c6b7eebfc7cf28c7a60318420f8fdd6494a282
cjh5414 <cjh5414@gmail.com>
2016-10-05 16:06:11 +0900

    퍼소나를 이용한 사용자 정의 인증 처리 스파이크

--- lists/static/lists.js
+++ lists/static/lists.js
@@ -1,5 +1,13 @@
+/*
 $(function(){
     $('input').keypress(function(){
+        $('.has-error').hide();
+    });
+});
+*/
+
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function () {
         $('.has-error').hide();
     });
 });
