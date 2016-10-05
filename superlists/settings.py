a851ff64a05e914c992c7cf1993387feb13cc617
cjh5414 <cjh5414@gmail.com>
2016-10-05 15:07:07 +0900

    서버 인증 커스터마이징

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
